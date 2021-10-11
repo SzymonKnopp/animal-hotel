a7618e1848a8f83631a27727ace1e33adbf6c478
Szymon Knopp <knopp.s99@gmail.com>
2021-10-11 20:35:02 +0200

    start.spring.io

---
+++ src/main/java/s175550/animalhotel/AnimalHotelApplication.java
@@ -0,0 +1,13 @@
+package s175550.animalhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalHotelApplication.class, args);
+    }
+
+}
