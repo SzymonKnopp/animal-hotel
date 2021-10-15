27d60f0bed04e11f640d97768b44536768501e71
Szymon Knopp <knopp.s99@gmail.com>
2021-10-15 21:10:27 +0200

    getCommand

--- src/main/java/s175550/animalhotel/UserInterface.java
+++ src/main/java/s175550/animalhotel/UserInterface.java
@@ -11,6 +11,14 @@ public class UserInterface {
     private final Scanner scanner;
     private final PrintStream print;
 
+    public enum Command {
+        LIST_ENTITY_CATEGORIES,
+        LIST_ENTITIES_FROM_CATEGORY,
+        ADD_NEW_ENTITY,
+        DELETE_ENTITY,
+        STOP_APPLICATION
+    }
+
     public UserInterface(PrintStream print, InputStream input) {
         this.print = print;
         this.scanner = new Scanner(input);
@@ -30,10 +38,22 @@ public class UserInterface {
         print.println("e - list entities from category");
         print.println("a - add new entity");
         print.println("d - delete entity");
+        print.println("x - stop application");
     }
 
     public void displayCategories() {
         print.println("1. Owner");
         print.println("2. Animal");
+    }
+
+    public Command getCommand() throws IllegalArgumentException {
+        switch (scanner.nextLine()) {
+            case "c": return Command.LIST_ENTITY_CATEGORIES;
+            case "e": return Command.LIST_ENTITIES_FROM_CATEGORY;
+            case "a": return Command.ADD_NEW_ENTITY;
+            case "d": return Command.DELETE_ENTITY;
+            case "x": return Command.STOP_APPLICATION;
+            default: throw new IllegalArgumentException("invalid command");
+        }
     }
 }
