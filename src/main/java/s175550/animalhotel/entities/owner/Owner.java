67080c93cefc469ad029fd4b315bfdd4894a4055
Szymon Knopp <knopp.s99@gmail.com>
2021-10-21 19:13:05 +0200

    JPA annotations for entities

--- src/main/java/s175550/animalhotel/entities/owner/Owner.java
+++ src/main/java/s175550/animalhotel/entities/owner/Owner.java
@@ -3,20 +3,46 @@ package s175550.animalhotel.entities.owner;
 import lombok.*;
 import s175550.animalhotel.UserInterface;
 import s175550.animalhotel.entities.Gender;
+import s175550.animalhotel.entities.animal.Animal;
 
+import javax.persistence.*;
 import java.io.PrintStream;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Scanner;
 
 @Builder
-@AllArgsConstructor (access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @ToString
-@EqualsAndHashCode
+
+@Entity
+@Table(name = "owners")
 public class Owner implements Serializable {
-    private final String name;
-    private final String surname;
-    private final Gender gender;
+    @ToString.Exclude
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String surname;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "owner")
+    @Column(name = "owned_animals")
+    private List<Animal> ownedAnimals;
+
+
+    private Owner(String name, String surname, Gender gender) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
 
     public static Owner createOwnerFromUI(UserInterface ui) throws NumberFormatException {
         Scanner scanner = ui.getScanner();
