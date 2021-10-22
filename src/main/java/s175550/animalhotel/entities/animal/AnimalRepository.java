d9299304ada1e63375f157ecfd9e7750a8e32491
Szymon Knopp <szymon.knopp@appfire.com>
2021-10-22 19:07:29 +0200

    remove generic Repository

--- src/main/java/s175550/animalhotel/entities/animal/AnimalRepository.java
+++ src/main/java/s175550/animalhotel/entities/animal/AnimalRepository.java
@@ -1,15 +1,51 @@
 package s175550.animalhotel.entities.animal;
 
-import lombok.NoArgsConstructor;
-import s175550.animalhotel.Repository;
+
+import org.springframework.stereotype.Repository;
+import s175550.animalhotel.CloningUtility;
 import s175550.animalhotel.entities.owner.Owner;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
-@NoArgsConstructor
-@org.springframework.stereotype.Repository
-public class AnimalRepository extends Repository<Animal> {
+@Repository
+public class AnimalRepository{
+    private final HashMap<Integer, Animal> storage;
+
+    public AnimalRepository() {
+        storage = new HashMap<>();
+    }
+
+    public void save(Animal entity) throws IllegalArgumentException {
+        int key = storage.size();
+        if(storage.containsKey(key)) {
+            throw new IllegalArgumentException("attempted insertion of duplicate key");
+        }
+        storage.put(key, entity);
+    }
+
+    public Optional<Animal> findById(Integer key) {
+        return Optional.ofNullable(CloningUtility.clone(storage.get(key)));
+    }
+
+    public List<Animal> findAll() {
+        List<Animal> result = new ArrayList<>();
+        storage.values().forEach(item -> result.add(CloningUtility.clone(item)));
+        return result;
+    }
+
+    public void deleteById(Integer key) {
+        storage.remove(key);
+    }
+
+    public void delete(Animal entityToDelete) {
+        List<Integer> keysToDelete = storage.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(entityToDelete))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        if (keysToDelete.size() == 1) this.deleteById(keysToDelete.get(0));
+    }
 
     public List<Animal> getAllByOwner(Owner owner) {
         return this.findAll().stream().filter(animal -> animal.getOwner().equals(owner)).collect(Collectors.toList());
--- src/main/java/s175550/animalhotel/entities/owner/OwnerRepository.java
+++ src/main/java/s175550/animalhotel/entities/owner/OwnerRepository.java
@@ -1,10 +1,48 @@
 package s175550.animalhotel.entities.owner;
 
 
-import lombok.NoArgsConstructor;
-import s175550.animalhotel.Repository;
+import org.springframework.stereotype.Repository;
+import s175550.animalhotel.CloningUtility;
 
-@NoArgsConstructor
-@org.springframework.stereotype.Repository
-public class OwnerRepository extends Repository<Owner> {
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Repository
+public class OwnerRepository {
+    private final HashMap<Integer, Owner> storage;
+
+    public OwnerRepository() {
+        storage = new HashMap<>();
+    }
+
+    public void save(Owner entity) throws IllegalArgumentException {
+        int key = storage.size();
+        if(storage.containsKey(key)) {
+            throw new IllegalArgumentException("attempted insertion of duplicate key");
+        }
+        storage.put(key, entity);
+    }
+
+    public Optional<Owner> findById(Integer key) {
+        return Optional.ofNullable(CloningUtility.clone(storage.get(key)));
+    }
+
+    public List<Owner> findAll() {
+        List<Owner> result = new ArrayList<>();
+        storage.values().forEach(item -> result.add(CloningUtility.clone(item)));
+        return result;
+    }
+
+    public void deleteById(Integer key) {
+        storage.remove(key);
+    }
+
+    public void delete(Owner entityToDelete) {
+        List<Integer> keysToDelete = storage.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(entityToDelete))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        if (keysToDelete.size() == 1) this.deleteById(keysToDelete.get(0));
+    }
 }
