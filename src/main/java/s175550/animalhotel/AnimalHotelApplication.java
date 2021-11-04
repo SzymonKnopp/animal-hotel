b466e2021d6a4575fc1c670e354c7fb421125108
Szymon Knopp <knopp.s99@gmail.com>
2021-11-04 13:54:43 +0100

    add gateway with routing

--- src/main/java/s175550/animalhotel/AnimalHotelApplication.java
+++ src/main/java/s175550/animalhotel/AnimalHotelApplication.java
@@ -1,14 +1,35 @@
 package s175550.animalhotel;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AnimalHotelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalHotelApplication.class, args);
-
     }
 
+    @Bean
+    public RouteLocator routeLocator(
+            RouteLocatorBuilder builder,
+            @Value("${server.port}") Integer port,
+            @Value("${animal-hotel.owners.url}") String ownersUrl,
+            @Value("${animal-hotel.animals.url}") String animalsUrl
+    ) {
+        return builder.routes()
+                .route("owners", route -> route.host("localhost:" + port)
+                        .and()
+                        .path("/api/owners", "/api/owners/**")
+                        .uri(ownersUrl))
+                .route("animals", route -> route.host("localhost:" + port)
+                        .and()
+                        .path("/api/animals", "/api/animals/**")
+                        .uri(animalsUrl))
+                .build();
+    }
 }
